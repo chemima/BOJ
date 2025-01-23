d6c290b239699a17b26b6c012c94abe72836dbf5
chemi_ma <kcw1537@postech.ac.kr>
2025-01-24 02:25:22 +0900

    [Silver IV] Title: 듣보잡, Time: 76 ms, Memory: 42536 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.py
+++ 백준/Silver/1764. 듣보잡/듣보잡.py
@@ -1,14 +1,17 @@
+import sys
+input = sys.stdin.readline
+
 a, b = map(int,input().split())
-non_hear = []
-non_see = []
+non_hear = [0] * a
+non_see = [0] * b
 
 for i in range(a):
-    person = input()
-    non_hear.append(person)
+    person = input().strip()
+    non_hear[i] = person
     
 for i in range(b):
-    person = input()
-    non_see.append(person)
+    person = input().strip()
+    non_see[i] = person
     
 non_hear_see = list(set(non_hear).intersection(non_see))
 non_hear_see.sort()
